68f21bb43a66809ad23b0f36072071e67a9bc1ca
Felix <30987125+flxcn@users.noreply.github.com>
2021-05-03 19:35:06 -0500

    Update bottom_up_scraper.py

--- bottom_up_scraper.py
+++ bottom_up_scraper.py
@@ -1,56 +1,71 @@
 import requests
 from bs4 import BeautifulSoup
+from treelib import Node, Tree
 
 # initialize empty list of family members
-familia = []
+familia = Tree()
+existing_personae = set()
+print("Enter the URL of your base article:")
+base_article_url = input()
 
 # recursive function to scrape a Wikipedia article
-def findParents(url,generation):
+def findParents(url,generation,child_article):
     response = requests.get(url=url)
     soup = BeautifulSoup(response.content, 'html.parser')
-
     title = soup.find(id="firstHeading")
 
-    familia.append(title.text + ", Generation:" + str(generation))
+    # familia.append(title.text + ", Generation:" + str(generation))
 
-    infoboxLabels = soup.find_all('th',{"class":"infobox-label"})
-    for label in infoboxLabels:
+    if url not in existing_personae:
+        print(title.text)
+        existing_personae.add(url)
 
-        if "Father" in label.text:
-            fathers = label.find_next().find_all("a")
-            for father in fathers:
-                # check to ensure only Wikipedia links are followed
-                if father['href'].find("/wiki/") == -1: 
-                    continue
+        if child_article == "":
+            familia.create_node(title.text,url)
+        else:
+            familia.create_node(title.text,url, parent=child_article)
 
-                # check to prevent duplicate entries, which may cause an infinite loop
-                if any(father.text in s for s in familia):
-                    continue
+        infoboxLabels = soup.find_all('th',{"class":"infobox-label"})
+        for label in infoboxLabels:
 
-                # go up one more generation
-                gen = generation
-                gen -= 1
-                findParents("https://en.wikipedia.org" + father['href'], gen)
+            if "Father" in label.text:
+                fathers = []
+                fathers = label.find_next().find_all("a")
+                for father in fathers:
+                    # check to ensure only Wikipedia links are followed
+                    if father['href'].find("/wiki/") == -1: 
+                        continue
 
-        if "Mother" in label.text:
-            mothers = label.find_next().find_all("a")
-            for mother in mothers:
-                # check to ensure only Wikipedia links are followed
-                if mother['href'].find("/wiki/") == -1: 
-                    continue
+                    # check to prevent duplicate entries, which may cause an infinite loop
+                    if any(father.text in s for s in existing_personae):
+                        continue
 
-                # check to prevent duplicate entries, which may cause an infinite loop
-                if any(mother.text in s for s in familia):
-                    continue
+                    # go up one more generation
+                    gen = generation
+                    gen -= 1
+                    findParents("https://en.wikipedia.org" + father['href'], gen, url)
 
-                # go up one more generation
-                gen = generation
-                gen -= 1
-                findParents("https://en.wikipedia.org" + mother['href'], gen)
+            if "Mother" in label.text:
+                mothers = []
+                mothers = label.find_next().find_all("a")
+                for mother in mothers:
+                    # check to ensure only Wikipedia links are followed
+                    if mother['href'].find("/wiki/") == -1: 
+                        continue
+
+                    # check to prevent duplicate entries, which may cause an infinite loop
+                    if any(mother.text in s for s in existing_personae):
+                        continue
+
+                    # go up one more generation
+                    gen = generation
+                    gen -= 1
+                    findParents("https://en.wikipedia.org" + mother['href'], gen, url)
                 
     
-findParents("https://en.wikipedia.org/wiki/Nero", 0)
+findParents(base_article_url, 0, "")
 
-# print out each family member
-for persona in familia:
-    print(persona)
+familia.show()
+# # print out each family member
+# for persona in familia:
+#     print(persona)
